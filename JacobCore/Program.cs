90d5ded6f436424393f1a0b9ea6a561d08189baa
Sanity Dance <mw@tdublu.com>
2021-06-28 09:01:33 -0700

    Added else clause to ExecuteInParallel so things actually execute.

--- JacobCore/Program.cs
+++ JacobCore/Program.cs
@@ -7,7 +7,7 @@ namespace JacobCore
 {
     class Program
     {
-        public static async Task<bool> ExecuteInParallel<T>(List<T> taskList, Func<T,Task<bool>> function, int throttle=0)
+        public static async Task<bool> ExecuteInParallel<T>(List<T> taskList, Func<T, Task<bool>> function, int throttle = 0)
         {
             bool allSucceeded = true;
             async Task doTasks(List<T> subList)
@@ -23,15 +23,19 @@ namespace JacobCore
                     allSucceeded = false;
                 }
             }
-            if(throttle!=0)
+            if (throttle != 0)
             {
                 int iteration = 0;
-                while(iteration * throttle < taskList.Count)
+                while (iteration * throttle < taskList.Count)
                 {
                     var currentTasks = taskList.Skip(iteration * throttle).Take(throttle).ToList();
                     await doTasks(currentTasks);
                     iteration++;
                 }
+            }
+            else
+            {
+                await doTasks(taskList);
             }
             return allSucceeded;
         }

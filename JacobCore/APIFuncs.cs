08a00c29d5e63a8f6de62a693d0460394efb0bf6
Sanity Dance <mw@tdublu.com>
2021-07-24 09:41:09 -0700

    Updated InvokeAPI to accept a tuple rather than an array of string. Also added an exception check to request execution.

--- JacobCore/APIFuncs.cs
+++ JacobCore/APIFuncs.cs
@@ -16,15 +16,15 @@ namespace JacobCore
         /// <summary>
         /// Invokes a REST API with the given parameters.
         /// </summary>
-        /// <param name="requestInfo">Array of string in the format ["VERB", "requestUrl"]</param>
+        /// <param name="requestInfo">Tuple in the format ("VERB", "requestUrl")</param>
         /// <param name="accessToken">Access token to authorize the request. Passed in headers as "Bearer accessToken" unless overrideAuthorizationHeader is true.</param>
         /// <param name="overrideAuthorizationHeader">If true, uses the entire accessToken string as the authorization header. For example, if it requires "Authorization accessToken", you would pass that instead of just the access token.</param>
         /// <param name="stringContent">String content for a POST or PUT request.</param>
         /// <param name="byteContent">Byte array content fr a POST or PUT request.</param>
         /// <returns>Tuple indicating success or failure with item 1 and response content in item 2.</returns>
-        public static async Task<(bool, JObject)> InvokeAPI(string[] requestInfo, string accessToken, bool overrideAuthorizationHeader = false, string stringContent = null, byte[] byteContent = null)
+        public static async Task<(bool, JObject)> InvokeAPI((string, string) requestInfo, string accessToken, bool overrideAuthorizationHeader = false, string stringContent = null, byte[] byteContent = null)
         {
-            using (var request = new HttpRequestMessage(new HttpMethod(requestInfo[0]), requestInfo[1]))
+            using (var request = new HttpRequestMessage(new HttpMethod(requestInfo.Item1), requestInfo.Item2))
             {
                 request.Headers.TryAddWithoutValidation("Accept", "application/json");
                 request.Headers.TryAddWithoutValidation("Authorization", overrideAuthorizationHeader ? accessToken : "Bearer " + accessToken);
@@ -37,12 +37,20 @@ namespace JacobCore
                 {
                     request.Content = new ByteArrayContent(byteContent);
                 }
-
-                var response = await httpClient.SendAsync(request);
+                HttpResponseMessage response;
+                try
+                {
+                    response = await httpClient.SendAsync(request);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Exception thrown while executing API request " + requestInfo.Item1 + ", " + requestInfo.Item2 + "\n" + ex.Message);
+                    return (false, new JObject() { "StatusCode", "418" });
+                }
 
                 bool success = response.IsSuccessStatusCode;
-                var contentString = await response.Content.ReadAsStringAsync();
-                var contentJson = JObject.Parse(string.IsNullOrWhiteSpace(contentString) ? "{}" : contentString);
+                string contentString = await response.Content.ReadAsStringAsync();
+                JObject contentJson = JObject.Parse(string.IsNullOrWhiteSpace(contentString) ? "{}" : contentString);
                 contentJson.Add(new JProperty("StatusCode", response.StatusCode));
 
                 return (success, contentJson);
@@ -61,11 +69,11 @@ namespace JacobCore
         {
             bool allSucceeded = true;
             string currentUrl = initialUri;
-            int itemsReturned = 0;
+            int itemsReturned;
             do
             {
                 itemsReturned = 0;
-                string[] currentRequest = { "GET", currentUrl };
+                (string, string) currentRequest = ( "GET", currentUrl );
                 var response = await APIFuncs.InvokeAPI(currentRequest, accessToken);
                 if (response.Item1)
                 {
